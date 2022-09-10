696758e3b03b25ddc07d1f92c15bc221b9920a45
tobycm <newtobycm@gmail.com>
2022-09-10 13:56:39 -0700

    fix length command

--- commands/Misc/length.js
+++ commands/Misc/length.js
@@ -1,23 +1,20 @@
-
-const db = require("quick.db");
+async function text_length(message, args) {
+    message.delete();
+    const full_text = args.join(" ");
+    if (!full_text)
+        return message.channel
+        .send("Nhập gì đi chứ 0.0")
+        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
+    return message.channel.send(`Độ dài chuỗi từ: \`${full_text.length}\``);
+}
 
 module.exports = {
-  name: "length",
-  category: "misc",
-  description: "Độ dài lệnh :/",
-  usage: "length <msg>",
-  args: true,
-  userPerms: ["SEND_MESSAGES"],
-    clientPerms:['EMBED_LINKS'],
-  run: async (client, message, args, del, member) => {
-    //const server_lang = await client.data.get(`serverlang_${message.guild.id}`);
-    //const lang = require(`../../language/${server_lang || "vi"}/misc/length.json`);
-    message.delete();
-    const usa = args.join(" ");
-    if (!usa)
-      return message.channel
-        .send(`${message.author}, ${lang.missingArgument}`)
-        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
-    return message.channel.send(`Độ dài chuỗi từ: \`${args.join(" ").length}\``);
-  }
+    name: "length",
+    category: "misc",
+    description: "Độ dài văn bản",
+    usage: "length <msg>",
+    args: true,
+    userPerms: ["SEND_MESSAGES"],
+    clientPerms:["SEND_MESSAGES"],
+    run: text_length
 };
