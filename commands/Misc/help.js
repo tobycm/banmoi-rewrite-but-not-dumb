79baf997a21daa4fad66480868e74fe654028961
tobycm <newtobycm@gmail.com>
2022-09-12 20:07:20 -0700

    almost there let's gooo

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -1,6 +1,36 @@
 const { MessageEmbed, MessageActionRow, MessageSelectMenu, Collection } = require('discord.js');
 const fs = require('fs');
 
+const emojis = { // an object which stores the emoji for each category for the menu options & embed
+    "info": "<:info:997673849246203945>",
+    "fun": "<:CommunityEntertainment:1000589373047324792>",
+    "Mod": "<:Moderator:1000588191113424927>",
+    "utility": "<a:utility:1000588711639121920>",
+    "Dev(Bot)":"<a:Developer:998796532537114654>",
+    "GiveAways":"<:giveaways:1000604650409558037>",
+    "Music":"<a:music:1000630934263050280>",
+    "Reaction":"<:MessageReaction:1000636651170709554>",
+    "Image":"<:image:1000637934967472168>",
+    "Feature":"<:feature:1000640613793923122>",
+    "Activity":"<:IconActivity:1000641239273709598>",
+    "Test":"<:Test:1000643427043655712>"
+};
+
+const description = { // an object which stores the description for each category for the embed
+    "info": "Lệnh về thông tin",
+    "utility": "Các lệnh lặt vặt ",
+    "fun": "Lệnh về trờ chơi hoặc tính năng thú vị",
+    "Mod": "Các lệnh cho người quản lí",
+    "Dev(Bot)":"Các lệnh đặc biệt(Chủ bot)",
+    "Music":"Lệnh về nhạc",
+    "Reaction":"Phản ứng",
+    "GiveAways":"Lệnh giveaways",
+    "Image":"Ảnh",
+    "Feature":"Tính năng",
+    "Activity":"Hoạt động trong voice>",
+    "TesT":"Cách tính năng đang trong quá trình thử nghiệm"
+};
+
 async function help(message, args) {
 
     let banmoi = message.client;
@@ -8,42 +38,11 @@ async function help(message, args) {
         var cate = new Collection();
         
         for (let command of banmoi.commands.entries()) {
-            let command_cate = cate.get(command.category).constructor === Array ? cate.get(command.category) : []
-            cate.set(command.category, command_cate)
+            let command_cate = cate.get(command.category).constructor === Array ? cate.get(command.category) : [];
+            cate.set(command.category, command_cate);
         }
-
-    
 
         let page = 0; // define the page variable as 0
-        const emojis = { // an object which stores the emoji for each category for the menu options & embed
-            "info": "<:info:997673849246203945>",
-            "fun": "<:CommunityEntertainment:1000589373047324792>",
-            "Mod": "<:Moderator:1000588191113424927>",
-            "utility": "<a:utility:1000588711639121920>",
-            "Dev(Bot)":"<a:Developer:998796532537114654>",
-            "GiveAways":"<:giveaways:1000604650409558037>",
-            "Music":"<a:music:1000630934263050280>",
-            "Reaction":"<:MessageReaction:1000636651170709554>",
-            "Image":"<:image:1000637934967472168>",
-            "Feature":"<:feature:1000640613793923122>",
-            "Activity":"<:IconActivity:1000641239273709598>",
-            "Test":"<:Test:1000643427043655712>"
-        };
-
-        const description = { // an object which stores the description for each category for the embed
-            "info": "Lệnh về thông tin",
-            "utility": "Các lệnh lặt vặt ",
-            "fun": "Lệnh về trờ chơi hoặc tính năng thú vị",
-            "Mod": "Các lệnh cho người quản lí",
-            "Dev(Bot)":"Các lệnh đặc biệt(Chủ bot)",
-            "Music":"Lệnh về nhạc",
-            "Reaction":"Phản ứng",
-            "GiveAways":"Lệnh giveaways",
-            "Image":"Ảnh",
-            "Feature":"Tính năng",
-            "Activity":"Hoạt động trong voice>",
-            "TesT":"Cách tính năng đang trong quá trình thử nghiệm"
-        }
 
         const menuOptions = [ // an array for the options of the dropdown menu (will push other options objects into it later)
             {
@@ -52,57 +51,73 @@ async function help(message, args) {
                 emoji: '🏠',
                 value: 'home'
             }
-        ]
+        ];
 
-        categories.forEach(cat => { // push all category name to "dirs" array
-            dirs.push(cat[0].name);
-        });
-    
         /* Help Embed */
         const embed = new MessageEmbed()
         .setColor('RED')
         .setTitle('Help Menu')
-        .setDescription(`Prefix của bot là: \`${prefix}\`\nSChọn vào mục bên dưới để xem tất cả cách lệnh sãn có`)
-
-        dirs.forEach((dir) => { // for each dir in the dirs array
-            embed.addField(
-                `${emojis[dir] || ''} ${dir.charAt(0).toUpperCase() + dir.slice(1).toLowerCase()}`, // field name included emoji and category name with first letter capitalized
-                `${description[dir] ? description[dir] : `${dir.charAt(0).toUpperCase() + dir.slice(1).toLowerCase()} Commands`}` // description taken from the description object, if the category isn't in the description object, then it will be a default text that we set
-            ) // add a field to the help menu home page.
-
-            menuOptions.push({ // push the menu select options for each category into the "menuOptions" array
-                label: `${dir}`, // label of the select menu option
-                description: `${dir} commands page`, // description of the select menu option
-                emoji: `${emojis[dir] || ''}`, // emoji of the select menu option
-                value: `${page++}` // the value of the select menu option which increase one for every select menu option using the ++ operator
-            })
-        });
-
-        const row = new MessageActionRow().addComponents( // create a new MessageActionRow and add components
-            new MessageSelectMenu() // make a new MessageSelectMenu
-            .setCustomId('select') // custom id of the select menu
-            .setPlaceholder('Click to see my category') // the placeholder of the select menu
-            .addOptions(menuOptions) // add the "menuOptions" array which includes all categories of select menu options
+        .setDescription(
+            `Prefix của bot là: \`${prefix}\`\n` +
+            `Chọn vào mục bên dưới để xem tất cả cách lệnh sãn có`
         );
 
-        var msg = await message.reply({ embeds: [embed], components: [row], fetchReply: true }); // send the embed with await for edit embed later
+        categories.forEach((cate, ) => {
+            let _name = cate.charAt(0).toUpperCase() + cate.slice(0).toLowerCase()
+            let emoji = emojis[cate] || ''
+            let name = emoji + " " + _name;
+            let desc = description[cate] ? description[cate] : _name;
+        
+            embed.addField(
+                name,
+                desc
+            )
 
-        const filter = i => !i.user.bot; // filter bot from using the dropdown menu
-        const collector = message.channel.createMessageComponentCollector({ // create a message component collector
-            filter, // apply the filter
-            componentType: 'SELECT_MENU', // type of the collector is select menu
-            time: 30000 // time of the collector is 30s
+            menuOptions.push(
+                { // push the menu select options for each category into the "menuOptions" array
+                    label: `${cate}`, // label of the select menu option
+                    description: `${cate} commands page`, // description of the select menu option
+                    emoji: emoji, // emoji of the select menu option
+                    value: `${page++}` // the value of the select menu option which increase one for every select menu option using the ++ operator
+                }
+            );
+
         });
 
+        const row = new MessageActionRow()
+        .addComponents( // create a new MessageActionRow and add components
+            new MessageSelectMenu() // make a new MessageSelectMenu
+                .setCustomId('select') // custom id of the select menu
+                .setPlaceholder('Click to see my category') // the placeholder of the select menu
+                .addOptions(menuOptions) // add the "menuOptions" array which includes all categories of select menu options
+        );
+
+        var msg = await message.reply(
+            {
+                embeds: [embed],
+                components: [row],
+                fetchReply: true
+            }
+        ); // send the embed with await for edit embed later
+
+        const filter = i => !i.user.bot; // filter bot from using the dropdown menu
+        const collector = message.channel.createMessageComponentCollector(
+            { // create a message component collector
+                filter, // apply the filter
+                componentType: 'SELECT_MENU', // type of the collector is select menu
+                time: 30000 // time of the collector is 30s
+            }
+        );
+
         collector.on('collect', async (i) => {
-            if(i.user.id !== message.author.id) return i.reply({ content: `This help page is not for you! Use the command \`${prefix}help\` yourself!`, ephemeral: true }); // if click menu user is not the message author then return an ephemeral message
+            if (i.user.id !== message.author.id) return i.reply({ content: `This help page is not for you! Use the command \`${prefix}help\` yourself!`, ephemeral: true }); // if click menu user is not the message author then return an ephemeral message
             i.deferUpdate(); // use this so your select menu won't load slowly when u select an option
 
             const value = i.values[0]; // the value that collector collects
 
-            if(i.customId !== 'select') return; // if collected value's component custom id is not equals to "select" then return
+            if (i.customId !== 'select') return; // if collected value's component custom id is not equals to "select" then return
 
-            if(value && value !== 'home') { // if there is value collected and the value is not equals to "home"
+            if (value && value !== 'home') { // if there is value collected and the value is not equals to "home"
                 embed.fields = []; // set the fields to empty
                 embed.setTitle(`${emojis[categories[value][0].name] ? emojis[categories[value][0].name] : ''} Help Menu | ${categories[value][0].name}`) // set the title as category name and the emoji in the front
 
@@ -117,7 +132,7 @@ async function help(message, args) {
                 msg = await msg.edit({ embeds: [embed], components: [row], fetchReply: true }); // edit the embed
             }
 
-            if(value === 'home') { // if the collected value is equals to "home"
+            if (value === 'home') { // if the collected value is equals to "home"
                 embed.fields = []; // set the embed fields to empty
                 embed.setTitle('Help Menu')
 
@@ -144,12 +159,12 @@ async function help(message, args) {
         let fields = []; // declare "fields" variable as empty array for the fields of the category help page
 
         fs.readdirSync('./commands/').forEach((dir) => { // for each folders inside commands folder
-            if(dir.toLowerCase() !== args.join(' ').toLowerCase()) return; // if folder name is not equals to the category you want to search in help command then return
+            if (dir.toLowerCase() !== args.join(' ').toLowerCase()) return; // if folder name is not equals to the category you want to search in help command then return
             const commands = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js')); // get the js files inside each category folder
 
             const cmds = commands.map((command) => { // map commands file
                 let file = require(`../../commands/${dir}/${command}`); // include the command file with the require() function
-                if(!file.name) return; // if the file did not export the command name, then return
+                if (!file.name) return; // if the file did not export the command name, then return
 
                 let name = file.name;
                 return { // return object with command name and description property
@@ -159,7 +174,7 @@ async function help(message, args) {
             });
 
             cmds.map(c => {
-                if(c == undefined) return; // if a command data is undefined then return
+                if (c == undefined) return; // if a command data is undefined then return
                 fields.push({ // push the fields of the embed to the "fields" array
                     name: `\`${prefix + c.name}\``, // prefix and the name of the command as field name
                     value: c.description ? c.description : 'No Description', // description of the command as field value
@@ -172,7 +187,7 @@ async function help(message, args) {
 
         const command = client.commands.get(args.join(' ').toLowerCase()) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(args.join(' ').toLowerCase())); // find if commands collection or aliases inside command data includes the arguments of your message
 
-        if(categs.includes(args.join(' ').toLowerCase())) { // if the categs array includes the category name you search
+        if (categs.includes(args.join(' ').toLowerCase())) { // if the categs array includes the category name you search
             const categEmbed = new MessageEmbed()
             .setTitle(`${args.join(' ').charAt(0).toUpperCase() + args.join(' ').slice(1)} Commands!`)
             .setDescription(`Use \`${prefix}help <command>\` to get details of a command.\nFor example: \`${prefix}help ping\`.\n\n`)
@@ -181,7 +196,7 @@ async function help(message, args) {
             return message.reply({ embeds: [categEmbed] });
         }
 
-        if(!command) { // if can't find that command from collection
+        if (!command) { // if can't find that command from collection
             const failEmbed = new MessageEmbed()
             .setDescription(`${message.author}, Invalid command! Use \`${prefix}help\` to see all mycommands!`)
             .setColor('RED')
@@ -189,13 +204,13 @@ async function help(message, args) {
         }
 
         const cmdEmbed = new MessageEmbed()
-        .setTitle('Command Details')
-        .addField('Command:', `${command.name}`) // command name
-        .addField('Aliases', command.aliases ? (command.aliases.length > 0 ? `\`${command.aliases.join("`, `")}\`` : 'No aliases') : 'No aliases') // if there is aliases array, then show, else show no aliases in the value of the field
-        .addField('Usage', command.usage ? `\`${prefix}${command.name} ${command.usage}\`` : `${prefix}${command.name}`) // if there is usage exported in command file, make prefix + command + usage as the value of the field, else just display prefix + command name
-        .addField('Command Description:', command.description ? command.description : 'No description') // if there is description of the command, then show in field value, else show "No description" in the field value
-        .setTimestamp()
-        .setColor('BLUE');
+            .setTitle('Command Details')
+            .addField('Command:', `${command.name}`) // command name
+            .addField('Aliases', command.aliases ? (command.aliases.length > 0 ? `\`${command.aliases.join("`, `")}\`` : 'No aliases') : 'No aliases') // if there is aliases array, then show, else show no aliases in the value of the field
+            .addField('Usage', command.usage ? `\`${prefix}${command.name} ${command.usage}\`` : `${prefix}${command.name}`) // if there is usage exported in command file, make prefix + command + usage as the value of the field, else just display prefix + command name
+            .addField('Command Description:', command.description ? command.description : 'No description') // if there is description of the command, then show in field value, else show "No description" in the field value
+            .setTimestamp()
+            .setColor('BLUE');
         return message.reply({ embeds: [cmdEmbed] });
     }
 
